53792b1f9143df21c66ef8a27001275e0064998b
sanchezalec321 <45341308+sanchezalec321@users.noreply.github.com>
2022-05-24 15:12:03 -0500

    updated db path

--- project/globals.py
+++ project/globals.py
@@ -3,8 +3,7 @@ import os
 title = "your app title"
 parent_directory = os.path.dirname(os.path.realpath(__file__))
 
-db_path = r"E:\Documents\Development\NPS_7600_Update\interface\Database\npsDB.db"
-
+db_path = ""
 changelog = [
     {
         'date': '18 Feb 2022',
